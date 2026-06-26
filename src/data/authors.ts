faa5f9878e05c1ec1e33516587fbe0163edf9393
Fieldry <wangzx@stu.pku.edu.cn>
2026-06-26 12:21:35 +0800

    fix: update author links for accuracy and completeness

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -1,6 +1,14 @@
 export const authorLinks: Record<string, string> = {
-  "Liantao Ma": "http://scholar.pku.edu.cn/malt/home",
-  "Yinghao Zhu": "https://yhzhu99.github.io/",
+  "Dehao Sui": "https://openreview.net/profile?id=~Dehao_Sui1",
+  "Huiya Zhao": "https://openreview.net/profile?id=~Huiya_Zhao1",
   "Junyi Gao": "https://aboutme.vixerunt.org/",
+  "Lei Gu": "https://openreview.net/profile?id=~Lei_Gu5",
   "Lequan Yu": "https://yulequan.github.io/",
+  "Liantao Ma": "http://scholar.pku.edu.cn/malt/home",
+  "Tianlong Wang": "https://openreview.net/profile?id=~Tianlong_Wang1",
+  "Wen Tang": "https://baike.baidu.com/item/%E5%94%90%E9%9B%AF/15893218",
+  "Weibin Liao": "https://openreview.net/profile?id=~Weibin_Liao1",
+  "Yifan Qi": "https://openreview.net/profile?id=~Yifan_Qi4",
+  "Yinghao Zhu": "https://yhzhu99.github.io/",
+  "Yasha Wang": "https://faculty.pku.edu.cn/wangyasha/en/index.htm",
 };
